f9733b7faba1fb47f6edba11457f5df6264b29f0
Simon Dittrich <sidit77@gmail.com>
2019-01-17 12:04:49 +0100

    Switched back to precompiled glfw binaries Basic glfw setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,33 @@
 
 int main( int argc, char** argv ){
 
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(1280, 720, "Shooter", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 }
