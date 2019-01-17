d5fd38d0ad7e56829afe6bebfa0136bbacc047fd
Simon Dittrich <sidit77@gmail.com>
2019-01-17 12:29:44 +0100

    error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,35 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <exception>
+
+template <typename T>
+T throwonerror(T x, const char* error){
+    if(!x)
+        throw std::runtime_error(error);
+    return x;
+}
 
 int main( int argc, char** argv ){
 
     GLFWwindow* window;
+    try {
+        throwonerror(glfwInit(), "can't init glfw");
 
-    if (!glfwInit())
-        return -1;
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        throwonerror(window = glfwCreateWindow(1280, 720, "Shooter", NULL, NULL), "can't create window");
 
-    window = glfwCreateWindow(1280, 720, "Shooter", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
+        while (!glfwWindowShouldClose(window)) {
+
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+    }catch(const std::exception& e){
+        std::cout << "Error: " << std::endl << e.what() << std::endl;
+
     }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
     glfwTerminate();
-
 }
