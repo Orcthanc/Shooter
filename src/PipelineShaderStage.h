0faa6aff3e74993a0d335a621316313758757dff
Orcthanc <samuel@knoethig.net>
2019-03-06 22:17:21 +0100

    Started creating a rendering pipeline

---
+++ src/PipelineShaderStage.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <memory>
+#include <vector>
+
+namespace Shooter{
+	namespace Renderer{
+		typedef std::vector<char> ShaderCode;
+		
+		extern ShaderCode readFile( const std::string& path );
+		extern VkShaderModule createShaderModule( const VkDevice, const ShaderCode& );
+		extern VkShaderModule createShaderModule( const VkDevice, const std::string& path );
+
+		extern void createShaderStageInfos( const std::vector<VkShaderModule>& modules, const std::vector<VkShaderStageFlagBits>& stages, const std::vector<std::string>& entryPoints, std::vector<VkPipelineShaderStageCreateInfo>& result );
+	}
+}
